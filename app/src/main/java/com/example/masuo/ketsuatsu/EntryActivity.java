7751b3c2d98fd848cc7a82bdf4d3df2f871ca658
masuo <masuo.higuchi@gmail.com>
2016-03-24 00:01:33 +0900

    APIレベル変更

--- app/src/main/java/com/example/masuo/ketsuatsu/EntryActivity.java
+++ app/src/main/java/com/example/masuo/ketsuatsu/EntryActivity.java
@@ -1,14 +1,17 @@
 package com.example.masuo.ketsuatsu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+/**
+ * Created by Masuo on 2016/03/23.
+ */
+public class EntryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_entry);
     }
+
+
 }
